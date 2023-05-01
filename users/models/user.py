8b0028128e53fb5b033e7a13cf54673d1e9024e8
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-05-01 12:38:15 +0900

    update user model

--- users/models/user.py
+++ users/models/user.py
@@ -1,37 +1,12 @@
-from typing import Optional
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
-
-
-class UserQuerySet(BaseUserManager):
-    def get_by_id(self, user_id) -> Optional["User"]:
-        try:
-            return self.get(id=user_id)
-        except User.DoesNotExist:
-            return None
-
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        if not username:
-            raise ValueError("Users must have a username")
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
 
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
 
-    objects: UserQuerySet = UserQuerySet()
+    objects = UserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
