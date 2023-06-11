814fb5d7e3de21597a88b23e849bd34282899acc
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-06-11 10:24:52 +0900

    Change UserPreference.user to OneToOne

--- user_preferences/models/user_preference.py
+++ user_preferences/models/user_preference.py
@@ -14,7 +14,7 @@ class UserPreferenceQuerySet(models.QuerySet):
 
 
 class UserPreference(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     drink_whens = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
