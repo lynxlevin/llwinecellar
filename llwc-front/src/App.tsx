b4e3d839e3181c36091f70c5beb9cabef62ea25e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-07-25 14:54:33 +0900

    try react-router

--- llwc-front/src/App.tsx
+++ llwc-front/src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestPage from './testPage';
+import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        header
       </header>
+      <Routes>
+        <Route path="/" element={(
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Link to="/test">Click Me!</Link>
+          </>
+        )} />
+        <Route path="/test" element={
+          <TestPage />
+        } />
+      </Routes>
     </div>
   );
 }
---
+++ llwc-front/src/testPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestPage = () => {
+    return <p>Yay! This is my first react page</p>
+};
+
+export default TestPage;
