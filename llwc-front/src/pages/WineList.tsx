a62bd7437a59ecab32e1b1ef8fd378f1363e3fc3
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-07-28 12:04:09 +0900

    WineList page: change data to real wine data

--- llwc-front/src/pages/WineList.tsx
+++ llwc-front/src/pages/WineList.tsx
@@ -18,39 +18,81 @@ import { visuallyHidden } from '@mui/utils';
 
 // Originally copied from https://mui.com/material-ui/react-table/#sorting-amp-selecting
 
-interface Data {
+interface WineData {
+    id: string;
+    drink_when: string;
     name: string;
-    calories: number;
-    fat: number;
-    carbs: number;
-    protein: number;
+    producer: string;
+    country: string;
+    region_1: string;
+    region_2: string;
+    region_3: string;
+    region_4: string;
+    region_5: string;
+    cepage: string;
+    vintage: number;
+    bought_at: string;
+    bought_from: string;
+    price_with_tax: number;
+    drunk_at: string;
+    note: string;
 }
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number): Data {
+function createWineData(
+    id: string,
+    drink_when: string,
+    name: string,
+    producer: string,
+    country: string,
+    region_1: string,
+    region_2: string,
+    region_3: string,
+    region_4: string,
+    region_5: string,
+    cepage: string,
+    vintage: number | null,
+    bought_at: string,
+    bought_from: string,
+    price_with_tax: number,
+    drunk_at: string | null,
+    note: string,
+): WineData {
     return {
+        id,
+        drink_when,
         name,
-        calories,
-        fat,
-        carbs,
-        protein,
+        producer,
+        country,
+        region_1,
+        region_2,
+        region_3,
+        region_4,
+        region_5,
+        cepage,
+        vintage: vintage ?? 0,
+        bought_at,
+        bought_from,
+        price_with_tax,
+        drunk_at: drunk_at ?? '',
+        note,
     };
 }
 
-const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-];
+// prettier-ignore
+const wineRows = [
+    createWineData("cf7718e9-b7d3-404d-bd3c-ca28d1ade76a", "デイリー", "Le Prince", "Domaine de Roche Ville", "France", "Loire", "Saumur Champigny", "", "", "", "", 2017, "2023-04-18", "エノテカオンライン", 3080, null, ""),
+    createWineData("1928f721-3b98-4b5d-ba9a-26b323b558c0", "デイリー", "Village Pinot Noir", "Paul Cluver", "South Africa", "Elgin", "", "", "", "", "", 2020, "2023-04-18", "エノテカオンライン", 2805, null, ""),
+    createWineData("83b4afc9-584e-4da5-a5f0-2feb0be75fe4", "そのうち飲む", "Aloxe Corton", "Domaine Charlopin Tissier", "France", "Bourgogne", "Aloxe Corton", "", "", "", "", 2018, "2022-11-26", "伊勢屋", 11880, null, ""),
+    createWineData("2d3113d2-83b1-420b-baa8-7d42607f6704", "そのうち飲む", "Cotes de nuits Village", "Domaine Charlopin Tissier", "France", "Bourgogne", "Cotes de nuits", "", "", "", "", 2018, "2022-11-26", "伊勢屋", 7700, null, ""),
+    createWineData("9d48576a-7a0c-4d8d-9997-8f124633628c", "数年寝かす", "Chasse Spleen", "Ch. Chasse Spleen", "France", "Bordeaux", "Médoc", "Moulis", "", "", "", 2013, "2019-06-15", "天満屋", 5724, null, "澱はなく、色は濃いめで透き通っている。"),
+    createWineData("7d392fc8-a12b-473f-a76e-e0cca7c03d12", "10年弱寝かす", "Charmes Chambertin", "Lou Dumont", "France", "Bourgogne", "Gevrey Chambertin", "Charmes Chambertin", "", "", "", 2016, "2020-08-05", "天満屋岡山", 29590, null, ""),
+    createWineData("720f50b2-f95b-4dfd-82de-c0802631e417", "10年強寝かす", "Gevrey Chambertin Lavaux St Jacques 1er", "Claude Dugat", "France", "Bourgogne", "Gevrey Chambertin", "Lavaux st Jacques", "", "", "", 2016, "2019-06-15", "勝田商店", 30780, null, "澱はなく、色はどちらかというと濃いめ。長期熟成が期待できそう。よく透き通っている。"),
+    createWineData("fbfac37c-c960-4c03-8d29-ce6749341daa", "たくさん寝かす", "Corton Bressandes GC", "Lucien le Moine", "France", "Bourgogne", "Corton", "", "", "", "", 2012, "2019-06-15", "勝田商店", 17280, null, "綿状の澱僅かにあり、色は微妙に薄め。よく透き通っている。"),
+    createWineData("6038fe2c-9aa4-4b3f-812e-aee9537aed0a", "たくさん寝かす", "Chambertin", "Phillipe Charlopin", "France", "Bourgogne", "Gevrey Chambertin", "Chambertin", "", "", "", 2012, "2016-11-05", "安田", 41040, null, "\"同じ物を2017/05/14に飲んでがっかり。確かに早飲みできるものだし、チャーミングな香りも程よい甘さも、ミネラル感や複雑味もあるが、どれもこぢんまりとしていて、全く魅力に感じられない。\nどう熟成するのか読めないが、偉大な畑の片鱗を感じることはできるので、もっとしっかり寝かせて、古酒になりかけてから飲むべき。少しでも早かったら絶対にまたがっかりする。\nこんなにすっと口に馴染んで、優しく飲みやすいワインにしては珍しく、しっかりと強固な刺激も備えている。今でこそ、口に優しくおいしいワインだが、ルロワにこそ叶わないかもしれないけど、熟成すれば閉じたように見えない花が大きく開いて、素晴らしいワインになるのかもしれない。\n死ぬ前の楽しみにとっておく。\""),
+    createWineData("34fdd25f-9446-4a6e-b1ec-f1232682fbc9", "", "Chateau Hortevie", "Chateau Hortevie", "France", "Bordeaux", "Médoc", "Saint-Julien", "", "", "", 2002, "2023-03-25", "タカムラ", 4400, "2023-05-05", ""),
+    createWineData("94194356-eb78-41a5-af5a-aea5532b181a", "", "Crozes Hermitage", "E. Guigal", "France", "Côtes du Rhône", "Septentrional", "Crozes Hermitage", "", "", "", 2019, "2023-03-25", "タカムラ", 3058, "2023-04-15", ""),
+    createWineData("13fff526-2fad-4e5c-a0ac-f3cb4f1ac83e", "", "Te Tera", "Martinborough Vineyartd", "New Zealand", "Wairarapa", "Martinborough", "", "", "", "", 2020, "2023-03-25", "タカムラ", 2780, "2023-04-08", ""),
+  ]
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
     if (b[orderBy] < a[orderBy]) {
@@ -71,78 +113,67 @@ function getComparator<Key extends keyof any>(
     return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map(el => el[0]);
-}
-
-interface HeadCell {
-    disablePadding: boolean;
-    id: keyof Data;
-    label: string;
+interface WineHeadCell {
+    id: keyof WineData;
     numeric: boolean;
+    disablePadding: boolean;
+    label: string;
 }
 
-const headCells: readonly HeadCell[] = [
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: false,
-        label: 'Dessert (100g serving)',
-    },
-    {
-        id: 'calories',
-        numeric: true,
-        disablePadding: false,
-        label: 'Calories',
-    },
-    {
-        id: 'fat',
-        numeric: true,
-        disablePadding: false,
-        label: 'Fat (g)',
-    },
-    {
-        id: 'carbs',
-        numeric: true,
-        disablePadding: false,
-        label: 'Carbs (g)',
-    },
-    {
-        id: 'protein',
-        numeric: true,
-        disablePadding: false,
-        label: 'Protein (g)',
-    },
+function toTitleCase(text: string): string {
+    return text
+        .toLowerCase()
+        .split('_')
+        .map(function (word: string) {
+            return word.replace(word[0], word[0].toUpperCase());
+        })
+        .join(' ');
+}
+
+function getWineHeadCell(id: keyof WineData, numeric: boolean, disablePadding: boolean): WineHeadCell {
+    const label = toTitleCase(id);
+    return {
+        id,
+        numeric,
+        disablePadding,
+        label,
+    };
+}
+
+const wineHeadCells: readonly WineHeadCell[] = [
+    getWineHeadCell('name', false, false),
+    getWineHeadCell('drink_when', false, false),
+    getWineHeadCell('producer', false, false),
+    getWineHeadCell('country', false, false),
+    getWineHeadCell('region_1', false, false),
+    getWineHeadCell('region_2', false, false),
+    getWineHeadCell('region_3', false, false),
+    getWineHeadCell('region_4', false, false),
+    getWineHeadCell('region_5', false, false),
+    getWineHeadCell('cepage', false, false),
+    getWineHeadCell('vintage', true, false),
+    getWineHeadCell('bought_at', false, false),
+    getWineHeadCell('bought_from', false, false),
+    getWineHeadCell('price_with_tax', true, false),
+    getWineHeadCell('drunk_at', false, false),
 ];
 
 interface EnhancedTableHeadProps {
-    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof WineData) => void;
     order: Order;
     orderBy: string;
 }
 
 function EnhancedTableHead(props: EnhancedTableHeadProps) {
     const { order, orderBy, onRequestSort } = props;
-    const createSortHandler = (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+    const createSortHandler = (property: keyof WineData) => (event: React.MouseEvent<unknown>) => {
         onRequestSort(event, property);
     };
 
     return (
         <TableHead>
             <TableRow>
-                {headCells.map(headCell => (
+                {wineHeadCells.map(headCell => (
                     <TableCell
                         key={headCell.id}
                         align={headCell.numeric ? 'right' : 'left'}
@@ -196,17 +227,18 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
 
 export default function WineList() {
     const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
+    const [orderBy, setOrderBy] = React.useState<keyof WineData>('drink_when');
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(100);
 
-    const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof Data) => {
+    const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof WineData) => {
         const isAsc = orderBy === property && order === 'asc';
         setOrder(isAsc ? 'desc' : 'asc');
         setOrderBy(property);
     };
 
-    const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const handleClick = (event: React.MouseEvent<unknown>, id: string, name: string) => {
+        console.log(id);
         console.log(name);
     };
 
@@ -220,10 +252,14 @@ export default function WineList() {
     };
 
     // Avoid a layout jump when reaching the last page with empty rows.
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - wineRows.length) : 0;
 
     const visibleRows = React.useMemo(
-        () => stableSort(rows, getComparator(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+        () =>
+            wineRows
+                .slice()
+                .sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
         [order, orderBy, page, rowsPerPage],
     );
 
@@ -241,7 +277,7 @@ export default function WineList() {
                                 return (
                                     <TableRow
                                         hover
-                                        onClick={event => handleClick(event, row.name)}
+                                        onClick={event => handleClick(event, row.id, row.name)}
                                         role="checkbox"
                                         tabIndex={-1}
                                         key={row.name}
@@ -250,10 +286,20 @@ export default function WineList() {
                                         <TableCell component="th" id={labelId} scope="row">
                                             {row.name}
                                         </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
-                                        <TableCell align="right">{row.fat}</TableCell>
-                                        <TableCell align="right">{row.carbs}</TableCell>
-                                        <TableCell align="right">{row.protein}</TableCell>
+                                        <TableCell align="right">{row.drink_when}</TableCell>
+                                        <TableCell align="right">{row.producer}</TableCell>
+                                        <TableCell align="right">{row.country}</TableCell>
+                                        <TableCell align="right">{row.region_1}</TableCell>
+                                        <TableCell align="right">{row.region_2}</TableCell>
+                                        <TableCell align="right">{row.region_3}</TableCell>
+                                        <TableCell align="right">{row.region_4}</TableCell>
+                                        <TableCell align="right">{row.region_5}</TableCell>
+                                        <TableCell align="right">{row.cepage}</TableCell>
+                                        <TableCell align="right">{row.vintage}</TableCell>
+                                        <TableCell align="right">{row.bought_at}</TableCell>
+                                        <TableCell align="right">{row.bought_from}</TableCell>
+                                        <TableCell align="right">{row.price_with_tax}</TableCell>
+                                        <TableCell align="right">{row.drunk_at}</TableCell>
                                     </TableRow>
                                 );
                             })}
@@ -271,7 +317,7 @@ export default function WineList() {
                 </TableContainer>
                 <TablePagination
                     component="div"
-                    count={rows.length}
+                    count={wineRows.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onPageChange={handleChangePage}
