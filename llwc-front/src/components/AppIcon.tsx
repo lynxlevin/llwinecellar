b9bad94fd9eb200678d0cb4563b188a9555ce03c
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-11-13 16:06:07 +0900

    Remove query from image for better caching.

--- llwc-front/src/components/AppIcon.tsx
+++ llwc-front/src/components/AppIcon.tsx
@@ -1,15 +1,7 @@
 import appIcon from '../assets/icon_295.png';
 
 const AppIcon = ({ height }: { height: number }) => {
-    return (
-        <img
-            srcSet={`${appIcon}?w=${height}&h=${height}&fit=crop&auto=format`}
-            src={`${appIcon}?w=${height}&h=${height}&fit=crop&auto=format`}
-            alt="app_icon"
-            height={height}
-            width={height}
-        />
-    );
+    return <img src={appIcon} alt="app_icon" height={height} width={height} />;
 };
 
 export default AppIcon;
