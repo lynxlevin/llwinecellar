c63244620650799540eec68936f5cba49bfbc6d8
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-12-03 22:23:52 +0900

    Add with_id flag

--- wines/management/commands/export_wines_as_csv.py
+++ wines/management/commands/export_wines_as_csv.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 
 from wines.models import Wine
 
@@ -9,36 +9,49 @@ FILE_DIR = "wines/fixtures"
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("--with-id", action="store_true")
+
     def handle(self, *args, **options):
+        """
+        python manage.py export_wines_as_csv
+        python manage.py export_wines_as_csv --with-id
+        """
+        with_id = options["with_id"]
+
         wines = Wine.objects.all()
-        text_lines = [
-            [
-                wine.cellarspace.cellar.name if hasattr(wine, "cellarspace") else None,
-                wine.position,
-                ", ".join(wine.tag_texts),
-                wine.name,
-                wine.producer,
-                wine.vintage,
-                wine.price,
-                wine.country.label if wine.country else None,
-                wine.region_1,
-                wine.region_2,
-                wine.region_3,
-                wine.region_4,
-                wine.region_5,
-                json.dumps(
-                    [
-                        {"name": c.name, "abbreviation": c.abbreviation, "percentage": str(c.percentage)}
-                        for c in wine.cepages.all()
-                    ]
-                ),
-                wine.bought_at,
-                wine.bought_from,
-                wine.drunk_at,
-                wine.note,
-            ]
-            for wine in wines
-        ]
+        text_rows = [self._get_row_for_wine(wine, with_id=with_id) for wine in wines]
         with open("exports/wines.csv", "w", encoding="utf_8") as f:
             writer = csv.writer(f)
-            writer.writerows(text_lines)
+            writer.writerows(text_rows)
+
+    def _get_row_for_wine(self, wine, with_id=False):
+        base_row = [
+            wine.cellarspace.cellar.name if hasattr(wine, "cellarspace") else None,
+            wine.position,
+            ", ".join(wine.tag_texts),
+            wine.name,
+            wine.producer,
+            wine.vintage,
+            wine.price,
+            wine.country.label if wine.country else None,
+            wine.region_1,
+            wine.region_2,
+            wine.region_3,
+            wine.region_4,
+            wine.region_5,
+            json.dumps(
+                [
+                    {"name": c.name, "abbreviation": c.abbreviation, "percentage": str(c.percentage)}
+                    for c in wine.cepages.all()
+                ]
+            ),
+            wine.bought_at,
+            wine.bought_from,
+            wine.drunk_at,
+            wine.note,
+        ]
+        if with_id:
+            base_row.insert(0, wine.id)
+
+        return base_row
--- wines/tests/management/commands/test_export_wines_as_csv.py
+++ wines/tests/management/commands/test_export_wines_as_csv.py
@@ -24,29 +24,31 @@ class TestExportWinesAsCsv(TestCase):
     def setUpTestData(cls):
         cls.user = UserFactory()
         cls.cellar = CellarFactory(user=cls.user)
-        cls.wines = [
-            WineInRackFactory(user=cls.user, cellar=cls.cellar, row=1, column=2),
-            WineFactory(user=cls.user),
-            WineFactory(user=cls.user, country=None),
-            DrunkWineFactory(user=cls.user),
-        ]
         cls.grapes = [
+            GrapeMasterFactory(user=cls.user),
             GrapeMasterFactory(user=cls.user, name="Cabernet Sauvignon", abbreviation="CS"),
             GrapeMasterFactory(user=cls.user, name="Merlot", abbreviation="Mr"),
         ]
-        CepageFactory(wine=cls.wines[0], grape=cls.grapes[0], percentage=60.0)
-        CepageFactory(wine=cls.wines[0], grape=cls.grapes[1], percentage=40.0)
-        cls.tags = WineTagFactory.create_batch(2, user=cls.user)
-        cls.wines[0].tags.set(cls.tags)
+        cls.tags = WineTagFactory.create_batch(5, user=cls.user)
 
     """
     MYMEMO:
-    - add with_id flag for not refresh_all in import.
+    - add with_header flag.
     - add import command: refresh_all flag, with caution message.
     - バッチ実行して、古いものの削除機能付きでバッチ実行もいいかも
     """
 
-    def test_export_for_all_users(self):
+    def test_export(self):
+        self.wines = [
+            WineInRackFactory(user=self.user, cellar=self.cellar, row=1, column=2),
+            WineFactory(user=self.user),
+            WineFactory(user=self.user, country=None),
+            DrunkWineFactory(user=self.user),
+        ]
+        CepageFactory(wine=self.wines[0], grape=self.grapes[1], percentage=60.0)
+        CepageFactory(wine=self.wines[0], grape=self.grapes[2], percentage=40.0)
+        self.wines[0].tags.set(self.tags[1::2])
+
         file_path = "exports/wines.csv"
 
         call_command("export_wines_as_csv")
@@ -55,34 +57,61 @@ class TestExportWinesAsCsv(TestCase):
             reader = csv.reader(f)
             exported_csv_rows = [row for row in reader]
 
-        expected_csv_rows = [
-            [
-                wine.cellarspace.cellar.name if hasattr(wine, "cellarspace") else "",
-                wine.position if hasattr(wine, "cellarspace") else "",
-                ", ".join(wine.tag_texts),
-                wine.name,
-                wine.producer,
-                str(wine.vintage),
-                str(wine.price),
-                wine.country.label if wine.country else "",
-                wine.region_1,
-                wine.region_2,
-                wine.region_3,
-                wine.region_4,
-                wine.region_5,
-                json.dumps(
-                    [
-                        {"name": c.name, "abbreviation": c.abbreviation, "percentage": str(c.percentage)}
-                        for c in wine.cepages.all()
-                    ]
-                ),
-                wine.bought_at.isoformat() if wine.bought_at else "",
-                wine.bought_from,
-                wine.drunk_at.isoformat() if wine.drunk_at else "",
-                wine.note,
-            ]
-            for wine in self.wines
-        ]
+        expected_csv_rows = [self._get_expected_row_fow_wine(wine) for wine in self.wines]
         self.assertEqual(expected_csv_rows, exported_csv_rows)
 
         os.remove(file_path)
+
+    def test_export_with_id(self):
+        self.wines = [
+            WineInRackFactory(user=self.user, cellar=self.cellar, row=1, column=2),
+            WineFactory(user=self.user),
+            WineFactory(user=self.user, country=None),
+            DrunkWineFactory(user=self.user),
+        ]
+        CepageFactory(wine=self.wines[0], grape=self.grapes[1], percentage=60.0)
+        CepageFactory(wine=self.wines[0], grape=self.grapes[2], percentage=40.0)
+        self.wines[0].tags.set(self.tags[1::2])
+
+        file_path = "exports/wines.csv"
+
+        call_command("export_wines_as_csv", "--with-id")
+
+        with open(file_path, "r", encoding="utf_8") as f:
+            reader = csv.reader(f)
+            exported_csv_rows = [row for row in reader]
+
+        expected_csv_rows = [self._get_expected_row_fow_wine(wine, with_id=True) for wine in self.wines]
+        self.assertEqual(expected_csv_rows, exported_csv_rows)
+
+        os.remove(file_path)
+
+    def _get_expected_row_fow_wine(self, wine, with_id=False):
+        base_row = [
+            wine.cellarspace.cellar.name if hasattr(wine, "cellarspace") else "",
+            wine.position if hasattr(wine, "cellarspace") else "",
+            ", ".join(wine.tag_texts),
+            wine.name,
+            wine.producer,
+            str(wine.vintage),
+            str(wine.price),
+            wine.country.label if wine.country else "",
+            wine.region_1,
+            wine.region_2,
+            wine.region_3,
+            wine.region_4,
+            wine.region_5,
+            json.dumps(
+                [
+                    {"name": c.name, "abbreviation": c.abbreviation, "percentage": str(c.percentage)}
+                    for c in wine.cepages.all()
+                ]
+            ),
+            wine.bought_at.isoformat() if wine.bought_at else "",
+            wine.bought_from,
+            wine.drunk_at.isoformat() if wine.drunk_at else "",
+            wine.note,
+        ]
+        if with_id:
+            base_row.insert(0, str(wine.id))
+        return base_row
