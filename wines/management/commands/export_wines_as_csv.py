243ad24d842b44c559cbb3fc9991fd736eb831eb
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-12-03 16:57:53 +0900

    Write easy params of 1 wine

--- wines/management/commands/export_wines_as_csv.py
+++ wines/management/commands/export_wines_as_csv.py
@@ -2,6 +2,8 @@ import csv
 
 from django.core.management.base import BaseCommand, CommandParser
 
+from wines.models import Wine
+
 FILE_DIR = "wines/fixtures"
 
 
@@ -11,8 +13,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # user_id = options["user_id"]
-        # MYMEMO: pass args to exporter class and save as csv
-        text_lines = [["abc", "def"], ["ghi"]]
+        wines = Wine.objects.all()
+        text_lines = [
+            [
+                wine.cellarspace.cellar.name,
+                wine.position,
+                # wine.tags,
+                wine.name,
+                wine.producer,
+                wine.vintage,
+                wine.price,
+                wine.country.label,
+                wine.region_1,
+                wine.region_2,
+                wine.region_3,
+                wine.region_4,
+                wine.region_5,
+                # wine.cepages,
+                wine.bought_at,
+                wine.bought_from,
+                wine.drunk_at,
+                wine.note,
+            ]
+            for wine in wines
+        ]
         with open("exports/wines.csv", "w", encoding="utf_8") as f:
             writer = csv.writer(f)
             writer.writerows(text_lines)
--- wines/tests/management/commands/test_export_wines_as_csv.py
+++ wines/tests/management/commands/test_export_wines_as_csv.py
@@ -8,11 +8,15 @@ from llwinecellar.common.test_utils import CellarFactory, DrunkWineFactory, User
 
 
 class TestExportWinesAsCsv(TestCase):
-    # @classmethod
-    # def setUpTestData(cls):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.cellar = CellarFactory(user=cls.user)
+        cls.wines = [WineInRackFactory(user=cls.user, cellar=cls.cellar, row=1, column=2)]
 
     def test_export_for_all_users(self):
         file_path = "exports/wines.csv"
+        wine = self.wines[0]
 
         call_command("export_wines_as_csv")
 
@@ -21,6 +25,31 @@ class TestExportWinesAsCsv(TestCase):
             exported_csv_rows = [row for row in reader]
 
         expected_csv_rows = [["abc", "def"], ["ghi"]]
+        expected_csv_rows = [
+            [
+                wine.cellarspace.cellar.name,
+                wine.position,
+                # wine.tags,
+                wine.name,
+                wine.producer,
+                str(wine.vintage),
+                str(wine.price),
+                wine.country.label,
+                wine.region_1,
+                wine.region_2,
+                wine.region_3,
+                wine.region_4,
+                wine.region_5,
+                # wine.cepages,
+                wine.bought_at.isoformat() if wine.bought_at else "",
+                wine.bought_from,
+                wine.drunk_at.isoformat() if wine.drunk_at else "",
+                wine.note,  # MYMEMO: add new lines and commas.
+            ]
+        ]
         self.assertEqual(expected_csv_rows, exported_csv_rows)
 
         os.remove(file_path)
+
+    # def test_export_per_user(self):
+    # def test_export_per_cellar(self):
